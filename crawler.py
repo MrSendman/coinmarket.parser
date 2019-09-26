44a9c058df9521bdc1696066bbcfc5ca19d37461
MrSendman <cenntyrion@gmail.com>
2019-09-26 21:53:35 +0300

    finnish project

--- crawler.py
+++ crawler.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
+from multiprocessing import Pool
 
 def get_html(url):
     r = requests.get(url)
@@ -38,19 +39,27 @@ def write_csv(data):
         writer.writerow( (data['name'],
                           data['price']) )
         print(data['name'], 'parsed')
+def make_all(url):
+    html = get_html(url)
+    data = get_page_data(html)
+    write_csv(data)
+
 
 def main():
     start = datetime.now()
     url = 'https://coinmarketcap.com/all/views/all/'
 
     all_links = get_all_links(get_html(url))
-    for url in all_links:
-        html = get_html(url)
-        data = get_page_data(html)
-        write_csv(data)
-    end = datetime.now()
-    total = end - start
-    print(str(total))
+
+    with Pool(40) as p:
+        p.map(make_all, all_links)
+    #for url in all_links:
+        #html = get_html(url)
+        #data = get_page_data(html)
+        #write_csv(data)
+    #end = datetime.now()
+    #total = end - start
+    #print(str(total))
 
 if __name__ == '__main__':
     main()
